257a336e3150ce1b5584ab06be31fab427d618e0
Marcin Bilski <gyamtso@gmail.com>
2022-05-11 18:19:48 +0200

    Generate the @font-face declaration.

--- lib/generators/icomoon_rails/update_generator.rb
+++ lib/generators/icomoon_rails/update_generator.rb
@@ -31,6 +31,18 @@ module IcomoonRails
       end
 
       def download_style_files
+          fo.write <<~END
+            @font-face {
+              font-family: 'icomoon';
+              src: url('/assets/icomoon_rails/icomoon.eot?5mo2a0');
+              src: url('/assets/icomoon_rails/icomoon.eot?5mo2a0#iefix') format('embedded-opentype'),
+              url('/assets/icomoon_rails/icomoon.ttf?5mo2a0') format('truetype'),
+              url('/assets/icomoon_rails/icomoon.woff?5mo2a0') format('woff'),
+              url('/assets/icomoon_rails/icomoon.svg?5mo2a0#icomoon') format('svg');
+              font-weight: normal;
+              font-style: normal;
+            }
+          END        
         File.open("#{assets_path}/icons.css", "wb") do |fo|
           fo.write css_body[css_body.index("}")+1..-1]
         end
